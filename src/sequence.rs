fa790859610f0609f8376b0e0200cb5d6d6a2538
Felix Girke <razorheadeffect@gmail.com>
2018-11-09 17:30:08 +0100

    Add sequence generation and its doctest

--- src/sequence.rs
+++ src/sequence.rs
@@ -28,16 +28,42 @@ pub fn expand(seed : u32) -> Vec<u8>{
 /// __Example__
 /// ```
 /// use aether_primitives::sequence;
-///
-///
-/// unimplemented!();
-/// ```
-pub fn generate(seed : Vec<u8>, generator : impl Fn(usize,&[u8]) -> u8, len : usize) -> Vec<u8>{
-    unimplemented!()
+/// /// This example generates one half of the pseudo-random sequence
+/// /// used by LTE's physical layer as per 3GPP TS36.211 7.2
+/// /// It is provided in the form
+/// /// ``` x(n+31) = (x(n+3) + x(n)) mod2 ```
+/// /// Since we use an array to generate that we need to indices
+/// /// Every step should yield one x(n) with n in [32..1600]
+/// /// ```x(n) = (x(n+3-31) + x(n-31)) mod2 ```
+/// /// Thus our generator is this
+/// let gen = |n : usize, seq : &[u8] | (seq[n-28] + seq[n-31]) % 2;
+/// let mut seed = sequence::expand(1u32);
+/// /// the sequence starts at n=32 thus we need to drop the last element
+/// seed.pop();
+/// let seq = sequence::generate(seed, gen, 1600);
+/// assert_eq!(seq.len(), 1600);
+/// /// not going to check these values here
+pub fn generate(mut seed : Vec<u8>, generator : impl Fn(usize,&[u8]) -> u8, len : usize) -> Vec<u8>{
+    while seed.len() < len {
+        let next_elem = generator(seed.len(), seed.as_ref());
+        seed.push(next_elem);
+    }
+    seed
 }
 
 #[cfg(test)]
 mod test{
     use crate::sequence;
+
+    #[test]
+    /// A simple sequence
+    fn simple_sequence(){
+        let gen = |n : usize, s : &[u8]| (s[n-1] + s[n-2]) % 2;
+
+        let seed = vec![1,0];
+        let seq = sequence::generate(seed, gen, 6);
+
+        assert_eq!(seq, vec![1,0,1,1,0,1]);
+    }
 
 }
