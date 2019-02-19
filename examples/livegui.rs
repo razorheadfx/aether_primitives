5067bd011239e2d4d59498b2b8c77eb8c651e4fe
Felix Girke <felix.girke@tu-dortmund.de>
2019-02-19 11:48:24 +0100

    Update livegui example

--- examples/livegui.rs
+++ examples/livegui.rs
@@ -1,5 +1,4 @@
-
-fn main(){
+fn main() {
     #[cfg(not(feature = "gui"))]
     #[cfg(not(feature = "fft"))]
     {
@@ -8,32 +7,39 @@ fn main(){
     }
 
     #[cfg(feature = "gui")]
-    #[cfg(feature = "fft")]{
+    #[cfg(feature = "fft")]
+    {
         use aether_primitives::cf32;
         use aether_primitives::channel::noise;
         use aether_primitives::gui;
         use aether_primitives::util::DB;
+        use std::f32::consts::PI;
         use std::sync::mpsc;
-        use std::time::{Duration, SystemTime};
         use std::thread;
-
+        use std::time::{Duration, SystemTime};
 
         let bins = 1024;
 
+        let (s, r) = mpsc::channel::<Vec<cf32>>();
 
-        let (s,r) = mpsc::channel::<Vec<cf32>>();
-
-        let w = gui::waterfall(bins,Some((-50.0,10.0)));
+        let w = gui::waterfall(bins, Some((-50.0, 10.0)));
         let h = gui::launch(r, w);
 
-        let mut noise = noise::new(DB(-0.0).ratio() as f32 ,815);
+        let mut noise = noise::new(DB(-20.0).ratio() as f32, 815);
 
         let duration = 10;
         let start = SystemTime::now();
-        while start.elapsed().unwrap().as_secs() < duration{
-            let v = noise.iter().take(bins).collect();
+        let mut f = 10e3;
+        while start.elapsed().unwrap().as_secs() < duration {
+            let v = (0..bins)
+                .map(|x| x as f32)
+                .map(|t| 2.0 * PI * f * t)
+                .map(|arg| cf32::from_polar(&1.0, &arg))
+                .zip(noise.iter())
+                .map(|(a, b)| a + b)
+                .collect::<Vec<_>>();
 
-            match s.send(v){
+            match s.send(v) {
                 Ok(_) => (),
                 Err(_) => {
                     println!("Receiver dropped; Did you click the exit button? Or Press Esc?");
@@ -41,8 +47,9 @@ fn main(){
                 }
             }
             thread::sleep(Duration::from_millis(100));
+            f += 100.0;
         }
-        drop(s);
+        drop(s); // drop the sender so the gui thread dies
         h.join().expect("Failed to rejoin the thread");
     }
 }
